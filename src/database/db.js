6f388ef09222b65b9c492d7739c3dc1a800a50f1
Dayane <dayanelaura3@gmail.com>
2022-11-29 20:37:01 -0300

    chore: adjust code for deploy on heroku

---
+++ src/database/db.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+let db; 
+
+try{
+    await mongoClient.connect();
+    db = mongoClient.db("drivencracy");
+    console.log("MangoDB Connected");
+}catch(err){
+    console.log(err);
+}
